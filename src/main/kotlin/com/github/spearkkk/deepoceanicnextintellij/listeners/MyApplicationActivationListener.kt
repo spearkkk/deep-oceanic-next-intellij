7c1f891eb67532eb521f664fa9e8d0bebf664cbb
GitHub Action <action@github.com>
2024-08-22 14:44:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/spearkkk/deepoceanicnextintellij/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/spearkkk/deepoceanicnextintellij/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.spearkkk.deepoceanicnextintellij.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
